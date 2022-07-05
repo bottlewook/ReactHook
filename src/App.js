14e5ac2469caa3a6cece9509f8ba5b0f3a9c4da0
임병욱 <imbyeong-ug@imbyeong-ug-ui-MacBookPro.local>
2022-07-05 11:03:36 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [names, setNames] = useState([]);
+  const [input, setInput] = useState("");
+  const handleinputChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const Uploadinput = () => {
+    setNames((prevState) => {
+      return [input, ...prevState];
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input value={input} onChange={handleinputChange} />
+      <button onClick={Uploadinput}>Upload</button>
+      {names.map((name, index) => (
+        <p key={index}>{name}</p>
+      ))}
     </div>
   );
 }
